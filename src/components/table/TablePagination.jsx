5233b6fea5aa72a066cad4f7267f92b2d3bac09f
jass2501 <jasmineagudomamani@gmail.com>
2026-05-13 21:31:08 +0200

    feature/table update styles and jsx

--- src/components/table/TablePagination.jsx
+++ src/components/table/TablePagination.jsx
@@ -1,65 +1,53 @@
 import "../../pages/tracker/table.css";
 
 function TablePagination({
-
   currentPage,
-
   totalPages,
-
   setCurrentPage,
-
 }) {
+  const maxVisible = 5;
+  const pages = [];
 
-  const pages =
-    [...Array(totalPages).keys()]
-      .map((number) => number + 1);
+  pages.push(1);
+
+  for (let i = 2; i <= Math.min(maxVisible, totalPages - 1); i++) {
+    pages.push(i);
+  }
+
+  if (totalPages > maxVisible + 1) {
+    pages.push("...");
+  }
+
+  if (totalPages > 1) {
+    pages.push(totalPages);
+  }
 
   return (
-
     <div className="pagination">
 
       <button
         disabled={currentPage === 1}
-        onClick={() =>
-          setCurrentPage(currentPage - 1)
-        }
+        onClick={() => setCurrentPage(currentPage - 1)}
       >
-
         Previous
-
       </button>
 
-      {pages.map((page) => (
-
+      {pages.map((page, index) => (
         <button
-          key={page}
-          className={
-            currentPage === page
-              ? "active-page"
-              : ""
-          }
-          onClick={() =>
-            setCurrentPage(page)
-          }
+          key={index}
+          disabled={page === "..."}
+          className={currentPage === page ? "active-page" : ""}
+          onClick={() => page !== "..." && setCurrentPage(page)}
         >
-
           {page}
-
         </button>
-
       ))}
 
       <button
-        disabled={
-          currentPage === totalPages
-        }
-        onClick={() =>
-          setCurrentPage(currentPage + 1)
-        }
+        disabled={currentPage === totalPages}
+        onClick={() => setCurrentPage(currentPage + 1)}
       >
-
         Next
-
       </button>
 
     </div>
