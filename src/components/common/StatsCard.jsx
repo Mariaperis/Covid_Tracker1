62d645c5b637f1c75c1d35c908256b417aa416c7
username <mtataje@beesion.com>
2026-05-13 15:29:12 +0200

    feat/update

--- src/components/common/StatsCard.jsx
+++ src/components/common/StatsCard.jsx
@@ -1,15 +1,23 @@
 import "./statsCard.css";
 
+
 function StatsCard({
   title,
   value,
   color,
+  icon,
 }) {
 
   return (
     <div className="stats-card">
 
+      <div className="stats-virus-icon">
+        <img src={icon} alt="virus-icon" />
+      </div>
+
       <h3>{title}</h3>
+
+      <div className="header-line"></div>
 
       <h2 className={color}>
         {value.toLocaleString()}
--- src/pages/tracker/Chats.jsx
+++ src/pages/tracker/Chats.jsx
@@ -5,6 +5,8 @@ import useCountries from "../../hooks/useCountries";
 import StatsCard from "../../components/common/StatsCard";
 
 import { getStatsConfig } from "../../utils/statsConfig";
+import LoadingDots from "../../components/common/LoadingDots";
+import FloatingActions from "../../components/common/FloatingActions";
 
 import "./chats.css";
 
@@ -33,36 +35,55 @@ function Chats() {
 
   return (
     <div className="right-panel">
+
       <div className="dashboard-header">
+
         <select
           className="country-select"
           value={selectedCountry}
           onChange={(e) => setSelectedCountry(e.target.value)}
         >
           {countriesAll.map((country) => (
-            <option key={country.country} value={country.country}>
+            <option
+              key={country.country}
+              value={country.country}
+            >
               {country.country}
             </option>
           ))}
         </select>
 
-        <p className="updated-date">Updated: June 5, 2022</p>
+        <p className="updated-date">
+          Updated: June 5, 2022
+        </p>
+
       </div>
 
       <div className="header-line"></div>
 
       <br />
 
-      <div className="stats-grid">
-        {stats.map((stat) => (
-          <StatsCard
-            key={stat.title}
-            title={stat.title}
-            value={stat.value}
-            color={stat.color}
-          />
-        ))}
+      <div className="stats-section">
+
+        <div className="stats-grid">
+
+          {stats.map((stat) => (
+            <StatsCard
+              key={stat.title}
+              title={stat.title}
+              value={stat.value}
+              color={stat.color}
+              icon={stat.icon}
+            />
+          ))}
+
+        </div>
+
+        <LoadingDots />
+        <FloatingActions />
+
       </div>
+
     </div>
   );
 }
