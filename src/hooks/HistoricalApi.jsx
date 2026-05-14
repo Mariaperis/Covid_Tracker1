18f66ee9a008bcaf0ac6cf28a287ae9c1793fdba
jass2501 <jasmineagudomamani@gmail.com>
2026-05-14 11:00:06 +0200

    add endpoint and update files

--- src/hooks/HistoricalApi.jsx
+++ src/hooks/HistoricalApi.jsx
@@ -1,31 +1,32 @@
 import { useState, useEffect } from "react";
+import { getHistorical } from "../api/endPoint";
 import { getTodayValue } from "../utils/Prueba";
 
 export function DateHook() {
-    const api_url_historical = import.meta.env.VITE_API_URL_HISTORICAL;
     const [historical, setHistorical] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const api = async (url) => {
+        const api = async () => {
             try {
-                const result = await fetch(url);
-                const covid_data = await result.json();
+                const covid_data = await getHistorical(); // already JSON
 
                 const parsed = covid_data.map(item => ({
                     country: item.country,
                     todayCases: getTodayValue(item.timeline, "cases"),
                     todayDeaths: getTodayValue(item.timeline, "deaths")
                 }));
+
                 setHistorical(parsed);
-                setLoading(false); 
+                setLoading(false);
 
             } catch (error) {
                 console.error("Error fetching API data:", error);
                 setLoading(false);
             }
         };
-        api(api_url_historical);
-    }, [api_url_historical]);
+        api();
+    }, []);
+
     return { historical, loading };
 }
